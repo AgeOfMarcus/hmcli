a36fb5412ddfb8b9071d0c32cfdc7ee17401d465
Marcus Weinberger <marcus@marcusweinberger.com>
2019-02-25 20:29:21 +0000

    first commit, working: crypto

---
+++ README.md
@@ -0,0 +1,2 @@
+# hmcli
+A command-line interface for the [hackerman](https://github.com/ageofmarcus/hackerman) library.
---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+import os
+
+hm_ver = "0.10.16"
+
+def read(fname):
+	return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setuptools.setup(name='hmcli',
+	version=hm_ver,
+	description='A command-line interface for the hackerman library.',
+	long_description=read("README.md"),
+	url='https://github.com/AgeOfMarcus/hmcli',
+	author='Marcus Weinberger',
+	author_email='marcus@marcusweinberger.com',
+	license='GPL',
+	packages=setuptools.find_packages(),
+	zip_safe=False,
+	install_requires=[
+		"hackerman>="+hm_ver,
+	])
